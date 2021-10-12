70b384048c0d94dbd778e51c82e08b14166029e4
Tushar Akhade <tusharakhade10@gmail.com>
2021-10-13 05:21:08 +0530

    Added: Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <div className="footerContainer">
+                <div className="footerContent">Copyright © 2020, Bookstore Private Limited. All Rights Reserved</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
