c23643de1212acc1482b233d6c2d46d3b774e4c9
Tushar Akhade <tusharakhade10@gmail.com>
2021-10-31 02:07:19 +0530

    Refactored: Remove comment

--- src/components/Router/ProtectedRoute.js
+++ src/components/Router/ProtectedRoute.js
@@ -3,14 +3,6 @@ import { Route, Redirect } from "react-router-dom"
 
 function ProtectedRoute({ component: Component, ...rest }) {
     return (
-        // <Route {...rest}
-        //     render={(props) => {
-        //         if(localStorage.getItem('bookStoreToken')) {
-        //             return <Component {...props} />
-        //         } else {
-        //             return <Redirect to="/" />
-        //         }
-        //     }} />
         <Route {...rest}
             render={(props) => {
                 return localStorage.getItem('bookStoreToken') ?
